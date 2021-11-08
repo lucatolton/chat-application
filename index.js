d0bd9611e8305b68882d8729e422444d6bb63964
lucatolton <41690024+lucatolton@users.noreply.github.com>
2021-11-08 18:37:16 +0000

    Initial push

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`[Chat] (Ready) :: Server running on port ${port}`);
+});
